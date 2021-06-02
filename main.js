5e8ceeecad806f96d37dd7cf39cca4737fa55f28
Vbrius <basirovyuri@gmail.com>
2021-06-03 00:53:44 +0800

    homework-1
    Не до конца разобрался с тем как добавлять в  о внутрь элемента второй класс, сегодня добью этот вопрос. Просто разница во времени у нас 6 часов)) Так что, у нас уже ночь - время для образования))

---
+++ main.js
@@ -0,0 +1,44 @@
+const player1 = {
+    name:'Scorpion',
+    hp = 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: 'sword',
+    attack: function{console.log('Scorpion' + 'Fight')}
+};
+const playur2 = {  
+    name:'Sub Zero',
+    hp = 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+    weapon: 'sword',
+    attack: function{console.log('Sub Zero' + 'Fight')}
+};
+
+function createPlayer(player,name,life)
+{
+const $player1 = document.createElement('div');
+
+$player1.classList.add(player);
+$progressbar.classList.add('progressbar');
+$life.classList.add('life');
+$naame1.classList.add('name');
+$character.classList.add('character');
+
+const $p = document.createElement('p');
+$p.innerText = name;
+
+const $img = document.createElement('img');
+
+$player1.appendChild($p);
+$player1.appendChild($img);
+
+
+const $root = document.querySelector('.root');
+$root.appendChild(player1);
+
+
+}
+
+createPlayer('player1', 'SCORPION', 50);
+createPlayer('player2', 'SUB-ZERO', 80);
+
+
